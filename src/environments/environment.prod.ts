302e84b4d9052d7b7003e77da80cd64393aba038
Hamed Baatour <hamedbaatour@gmail.com>
2019-08-18 23:28:40 +0300

    Minimus Part II Changes: https://bit.ly/2P0c86Z

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
-  appID: '671ac7a5d543c9935d09d1a2e6ebfb5b',
-  gKey: 'AIzaSyB674DWB6yR68yAr1RD8U4tAyCQj5K-ZG0',
+  appID: '',
+  gKey: '',
   config: {
-    apiKey: 'AIzaSyAoCbHJzrl2F5o0Xf1wvwH6SEAskVOZsMg',
-    authDomain: 'minimus-weather.firebaseapp.com',
-    databaseURL: 'https://minimus-weather.firebaseio.com',
-    projectId: 'minimus-weather',
-    storageBucket: 'minimus-weather.appspot.com',
-    messagingSenderId: '830228583555'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
   }
 };
