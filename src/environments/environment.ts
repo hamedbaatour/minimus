b386be1592efcf22ffcc47d7443448744bb6d2e0
janakas <janakas@codegen.net>
2021-05-29 23:00:22 +0530

    Configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,14 @@
 
 export const environment = {
   production: false,
-  appID: '',
+  appID: '9ee668a9ec66350adf0c20b47498656e',
   config: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: ''
+    apiKey: 'AIzaSyCgvxBiFZXLx-H2KSRCuICJQs2jqtwUAik',
+    authDomain: 'minimus-e61a6.firebaseapp.com',
+    databaseURL: 'https://minimus-e61a6-default-rtdb.firebaseio.com/',
+    projectId: 'minimus-e61a6',
+    storageBucket: 'minimus-e61a6.appspot.com',
+    messagingSenderId: '1016808871355'
   }
 };
 
