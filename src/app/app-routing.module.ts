c1dcf689adc67191470b4cee3abeebc7ab685863
Jamie Couperwhite <jcouperwhite@gmail.com>
2024-08-25 12:13:05 +0100

    update to angular v15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes, {})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
